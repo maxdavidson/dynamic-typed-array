a4c7069607cee30e6722291f4ea68b9b8fb9b167
Max Davidson <davidsonmax@gmail.com>
2016-01-17 02:12:36 +0100

    Fix indentation to not use tabs

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-	entry: 'dist-es2015/index.js',
+  entry: 'dist-es2015/index.js',
   dest: 'dist/dynamic-typed-array.js',
-	moduleName: 'DynamicTypedArray',
-	format: 'umd',
-	sourceMap: true,
-	plugins: [
-		babel()
-	]
+  moduleName: 'DynamicTypedArray',
+  format: 'umd',
+  sourceMap: true,
+  plugins: [
+    babel()
+  ]
 };
