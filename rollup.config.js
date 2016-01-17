1b2f8fbfd7b65d0d3892c8c9e3e0d04464e653cb
Max Davidson <davidsonmax@gmail.com>
2016-01-17 02:13:44 +0100

    Fix implicitly anonymous AMD export

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ export default {
   entry: 'dist-es2015/index.js',
   dest: 'dist/dynamic-typed-array.js',
   moduleName: 'DynamicTypedArray',
+  moduleId: 'dynamic-typed-array',
   format: 'umd',
   sourceMap: true,
   plugins: [
