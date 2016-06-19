5530fa15bdf42168ca94775c61b12a1e62942c11
Max Davidson <davidsonmax@gmail.com>
2016-06-20 01:12:40 +0200

    Add ES module build

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,14 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'dist-es2015/index.js',
-  dest: 'dist/dynamic-typed-array.js',
   moduleName: 'DynamicTypedArray',
   moduleId: 'dynamic-typed-array',
-  format: 'umd',
   sourceMap: true,
   plugins: [
     babel()
+  ],
+  targets: [
+    { dest: 'dist/dynamic-typed-array.js', format: 'umd' },
+    { dest: 'dist/dynamic-typed-array.es.js', format: 'es' }
   ]
 };
